c1453cd852b070e70413aa947710894bd4e11928
SBMitrovic <122743260+SBMitrovic@users.noreply.github.com>
2026-03-24 01:35:11 +0100

    initial commit

---
+++ src/test/java/com/example/taskforge_auth/TaskforgeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskforge_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskforgeAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
